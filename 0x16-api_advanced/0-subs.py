720fbc9382ee57274996fda5fa1bf5c4ad2acec4
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2023-06-18 17:56:39 +0000

    sub zero

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-""" n to query subscribers on a given Reddit subreddit."""
+""" Function that queries the Reddit API """
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """  Args:
+        subreddit: subreddit name
+    Returns:
+        number of subscribers to the subreddit,
+        or 0 if subreddit requested is invalid"""
+    headers = {'User-Agent': 'xica369'}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
-    	result = response.json().get("data")
-    	return result.get("subscribers")
-    else:
-        return (0)
+        return (response.json().get("data").get("subscribers"))
+    return (0)
